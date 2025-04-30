b9e68c10995c15d113dd224eb4c5befeec58aee6
alienHunterOnMars <alienHunterOnMars@protonmail.com>
2025-05-01 03:06:55 +0400

    added cmd verifier / worker scripts

---
+++ cmd/verifier/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DataDog/datadog-go/statsd"
+	"github.com/hibiken/asynq"
+	"github.com/sirupsen/logrus"
+
+	"github.com/vultisig/verifier/config"
+	"github.com/vultisig/verifier/internal/api"
+	"github.com/vultisig/verifier/internal/storage"
+	"github.com/vultisig/verifier/internal/storage/postgres"
+)
+
+func main() {
+	cfg, err := config.GetConfigure()
+	if err != nil {
+		panic(err)
+	}
+
+	logger := logrus.New()
+
+	sdClient, err := statsd.New(fmt.Sprintf("%s:%s", cfg.Datadog.Host, cfg.Datadog.Port))
+	if err != nil {
+		panic(err)
+	}
+
+	redisStorage, err := storage.NewRedisStorage(*cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	redisOptions := asynq.RedisClientOpt{
+		Addr:     cfg.Redis.Host + ":" + cfg.Redis.Port,
+		Username: cfg.Redis.User,
+		Password: cfg.Redis.Password,
+		DB:       cfg.Redis.DB,
+	}
+
+	client := asynq.NewClient(redisOptions)
+	defer func() {
+		if err := client.Close(); err != nil {
+			fmt.Println("fail to close asynq client,", err)
+		}
+	}()
+
+	inspector := asynq.NewInspector(redisOptions)
+	if cfg.Server.VaultsFilePath == "" {
+		panic("vaults file path is empty")
+
+	}
+	blockStorage, err := storage.NewBlockStorage(*cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := postgres.NewPostgresBackend(false, cfg.Server.Database.DSN)
+	if err != nil {
+		logger.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	server := api.NewServer(
+		cfg,
+		db,
+		redisStorage,
+		blockStorage,
+		redisOptions,
+		client,
+		inspector,
+		sdClient,
+		cfg.Server.VaultsFilePath,
+		cfg.Server.Mode,
+		cfg.Server.JWTSecret,
+		cfg.Server.Plugin.Type,
+		cfg.Server.Plugin.Eth.Rpc,
+		cfg.Plugin.PluginConfigs,
+		logger,
+	)
+	if err := server.StartServer(); err != nil {
+		panic(err)
+	}
+}
---
+++ cmd/worker/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DataDog/datadog-go/statsd"
+	"github.com/hibiken/asynq"
+	"github.com/sirupsen/logrus"
+
+	"github.com/vultisig/verifier/config"
+	"github.com/vultisig/verifier/internal/service"
+	"github.com/vultisig/verifier/internal/storage"
+	"github.com/vultisig/verifier/internal/syncer"
+	"github.com/vultisig/verifier/internal/tasks"
+)
+
+func main() {
+	cfg, err := config.GetConfigure()
+	if err != nil {
+		panic(err)
+	}
+
+	sdClient, err := statsd.New(cfg.Datadog.Host + ":" + cfg.Datadog.Port)
+	if err != nil {
+		panic(err)
+	}
+	blockStorage, err := storage.NewBlockStorage(*cfg)
+	if err != nil {
+		panic(err)
+	}
+	redisOptions := asynq.RedisClientOpt{
+		Addr:     cfg.Redis.Host + ":" + cfg.Redis.Port,
+		Username: cfg.Redis.User,
+		Password: cfg.Redis.Password,
+		DB:       cfg.Redis.DB,
+	}
+	logger := logrus.StandardLogger()
+	verifierConfig, err := config.ReadConfig("config-verifier")
+	if err != nil {
+		panic(err)
+	}
+	syncerService := syncer.NewPolicySyncer(logger.WithField("service", "syncer").Logger, verifierConfig.Server.Host, verifierConfig.Server.Port)
+	authService := service.NewAuthService(cfg.Server.JWTSecret)
+
+	client := asynq.NewClient(redisOptions)
+	inspector := asynq.NewInspector(redisOptions)
+
+	workerService, err := service.NewWorker(*cfg, verifierConfig.Server.Port, client, sdClient, syncerService, authService, blockStorage, inspector)
+	if err != nil {
+		panic(err)
+	}
+
+	srv := asynq.NewServer(
+		redisOptions,
+		asynq.Config{
+			Logger:      logger,
+			Concurrency: 10,
+			Queues: map[string]int{
+				tasks.QUEUE_NAME:         10,
+				tasks.EMAIL_QUEUE_NAME:   100,
+				"scheduled_plugin_queue": 10, // new queue
+			},
+		},
+	)
+
+	mux := asynq.NewServeMux()
+	mux.HandleFunc(tasks.TypeKeyGeneration, workerService.HandleKeyGeneration)
+	mux.HandleFunc(tasks.TypeKeySign, workerService.HandleKeySign)
+	mux.HandleFunc(tasks.TypeEmailVaultBackup, workerService.HandleEmailVaultBackup)
+	mux.HandleFunc(tasks.TypeReshare, workerService.HandleReshare)
+	mux.HandleFunc(tasks.TypePluginTransaction, workerService.HandlePluginTransaction)
+	mux.HandleFunc(tasks.TypeKeyGenerationDKLS, workerService.HandleKeyGenerationDKLS)
+	mux.HandleFunc(tasks.TypeKeySignDKLS, workerService.HandleKeySignDKLS)
+	mux.HandleFunc(tasks.TypeReshareDKLS, workerService.HandleReshareDKLS)
+	mux.HandleFunc(tasks.TypeMigrate, workerService.HandleMigrateDKLS)
+	if err := srv.Run(mux); err != nil {
+		panic(fmt.Errorf("could not run server: %w", err))
+	}
+}

84ba2ab45fb17e55d633bcd0fc0579ea767750c5
Raghav Sood <r@raghavsood.com>
2025-05-12 06:21:34 +0800

    plugin/registry: introduce plugin registry with constants for DCA and payroll

---
+++ plugin/registry.go
@@ -0,0 +1,13 @@
+package plugin
+
+type PluginID string
+
+// Each plugin must reserve a unique ID.
+//
+// A plugin ID is comprised of a all-lowercase string, ending with 4 random hex digits.
+//
+// Example: vultisig-dca-00a1
+const (
+	PluginVultisigDCA_0000     PluginID = "vultisig-dca-0000"
+	PluginVultisigPayroll_0000 PluginID = "vultisig-payroll-0000"
+)

4642a7b31cb670d619f01b69f256b8f27bd14380
Johnny Luo <johnnyluo1980@gmail.com>
2025-05-21 16:20:07 +1000

    refactor: remove vault password field from keysign structure

--- types/keysign.go
+++ types/keysign.go
@@ -11,7 +11,6 @@ type KeysignRequest struct {
 	HexEncryptionKey string   `json:"hex_encryption_key"` // Hex encryption key, used to encrypt the keysign messages
 	DerivePath       string   `json:"derive_path"`        // Derive Path
 	IsECDSA          bool     `json:"is_ecdsa"`           // indicate use ECDSA or EDDSA key to sign the messages
-	VaultPassword    string   `json:"vault_password"`     // password used to decrypt the vault file
 	Parties          []string `json:"parties"`            // parties to join the session
 	PluginID         string   `json:"plugin_id"`          // plugin id
 }

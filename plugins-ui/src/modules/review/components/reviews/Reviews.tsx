82ae0ff72346601c40614697b67ff173076ff48a
Rahul Mittal <84127070+SunshineRider@users.noreply.github.com>
2025-05-28 19:24:11 +0530

    Review fix (#104)
    * plugin_details page fix
    
    * review fix
    
    * add to gitignore
    
    * review fix
    
    * add to gitignore

--- plugins-ui/src/modules/review/components/reviews/Reviews.tsx
+++ plugins-ui/src/modules/review/components/reviews/Reviews.tsx
@@ -1,7 +1,6 @@
 import LeaveReview from "@/modules/review/components/leave-review/LeaveReview";
 import ReviewHistory from "@/modules/review/components/review-history/ReviewHistory";
 import "./Reviews.css";
-import { ReviewProvider } from "../../context/ReviewProvider";
 import { Plugin } from "@/modules/plugin/models/plugin";
 import Rating from "@/modules/shared/rating/Rating";
 
@@ -10,7 +9,7 @@ type ReviewProps = {
 };
 const Reviews = ({ plugin }: ReviewProps) => {
   return (
-    <ReviewProvider pluginId={plugin.id} ratings={plugin.ratings}>
+    <>
       <section>
         <h3 className="review-rating-header">Reviews and Ratings</h3>
         <div className="review-rating">
@@ -22,7 +21,7 @@ const Reviews = ({ plugin }: ReviewProps) => {
       <section className="reviews">
         <ReviewHistory />
       </section>
-    </ReviewProvider>
+    </>
   );
 };
 

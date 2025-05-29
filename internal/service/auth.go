e83520e7db006aea367abc3b86ce0ce56c3a6749
Raghav Sood <r@raghavsood.com>
2025-05-29 16:49:52 +0800

    server/auth: check for zero time

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -137,7 +137,7 @@ func (a *AuthService) ValidateToken(ctx context.Context, tokenStr string) (*Clai
 		return nil, errors.New("token not found in database")
 	}
 
-	if dbToken != nil && dbToken.RevokedAt.Before(time.Now()) {
+	if !dbToken.RevokedAt.IsZero() && dbToken.RevokedAt.Before(time.Now()) {
 		return nil, errors.New("token has been revoked")
 	}
 

2a814b5305e145170569cdd75e6e837d70f25810
Raghav Sood <r@raghavsood.com>
2025-06-02 04:44:52 +0800

    vault/config: add local party ID as config option

--- vault_config/config.go
+++ vault_config/config.go
@@ -4,6 +4,7 @@ type Config struct {
 	Relay struct {
 		Server string `mapstructure:"server" json:"server"`
 	} `mapstructure:"relay" json:"relay,omitempty"`
+	LocalPartyId     string `mapstructure:"local_party_id" json:"local_party_id,omitempty"`
 	QueueEmailTask   bool   `mapstructure:"queue_email_task" json:"queue_email_task,omitempty"`
 	EncryptionSecret string `mapstructure:"encryption_secret" json:"encryption_secret,omitempty"`
 }

125faf664b94d010f522d4ed5fba4dd9ce2c8d7d
Garry Sharp <>
2025-06-11 08:47:39 +0400

    More partial work

--- internal/storage/postgres/migrations/verifier/20250529070558_add_fees.sql
+++ internal/storage/postgres/migrations/verifier/20250529070558_add_fees.sql
@@ -1,6 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TYPE billing_frequency AS ENUM(
+    'daily',
+    'weekly',
+    'biweekly',
     'monthly'
 );
 
@@ -15,33 +18,73 @@ CREATE TABLE IF NOT EXISTS plugin_policy_billing(
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     type fee_type NOT NULL, -- Enum type for fee types
     frequency billing_frequency,
-    start_date int NOT NULL DEFAULT 1,
+    start_date DATE NOT NULL DEFAULT CURRENT_DATE, -- Start date of the billing cycle
+    amount int,
     plugin_policy_id uuid NOT NULL, -- Foreign key to plugin_policies
     CONSTRAINT fk_plugin_policy FOREIGN KEY (plugin_policy_id) REFERENCES plugin_policies(id) ON DELETE CASCADE,
-    CONSTRAINT only_first_of_month CHECK (start_date = 1)
+    CONSTRAINT frequency_check CHECK (
+        (type = 'recurring' AND frequency IS NOT NULL) OR
+        (type IN ('tx', 'once') AND frequency IS NULL)
+    )
 );
 
 CREATE TABLE IF NOT EXISTS fees(
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    type fee_type NOT NULL, -- Enum type for fee types
     plugin_policy_billing_id uuid NOT NULL, -- used for recurring fees
     transaction_id uuid, -- used for tx based fees only
-    billing_date date NOT NULL, -- used for tx based fees only
     amount bigint NOT NULL,
-    created_at timestamp DEFAULT now(),
+    created_at timestamp NOT NULL DEFAULT now(),
+    charged_at date NOT NULL DEFAULT now(),
     collected_at timestamp,
     CONSTRAINT fk_billing FOREIGN KEY (plugin_policy_billing_id) REFERENCES plugin_policy_billing(id) ON DELETE CASCADE
 );
+
+CREATE VIEW billing_periods AS 
+    SELECT pp.id as plugin_policy_id,
+    pp.active as active,
+    ppb.id as billing_id, 
+    ppb.frequency, 
+    ppb.amount, 
+    COUNT(f.id) as accrual_count,
+    COALESCE(SUM(f.amount),0) as total_billed,
+    COALESCE(MAX(f.charged_at), ppb.start_date) AS last_billed_date, 
+    COALESCE(MAX(f.charged_at), ppb.start_date) +
+    CASE ppb.frequency
+        WHEN 'daily' THEN INTERVAL '1 day'
+        WHEN 'weekly' THEN INTERVAL '1 week'
+        WHEN 'biweekly' THEN interval '2 weeks'
+        WHEN 'monthly' THEN INTERVAL '1 month'
+    END AS next_billing_date
+    from plugin_policy_billing ppb 
+    join plugin_policies pp on ppb.plugin_policy_id = pp.id
+    LEFT JOIN fees f ON f.plugin_policy_billing_id = ppb.id
+    WHERE ppb."type" = 'recurring'
+    GROUP BY (ppb.id, pp.id);
+
+CREATE VIEW fees_view AS 
+    SELECT pp.id AS policy_id, ppb.id AS billing_id, ppb."type", f.* 
+    FROM plugin_policies pp 
+    JOIN plugin_policy_billing ppb ON ppb.plugin_policy_id = pp.id 
+    JOIN fees f ON f.plugin_policy_billing_id = ppb.id;;
+
+CREATE INDEX idx_fees_plugin_policy_billing_id ON fees(plugin_policy_billing_id);
+CREATE INDEX idx_plugin_policy_billing_id ON plugin_policy_billing(id);
+CREATE INDEX idx_fees_transaction_id ON fees(transaction_id) WHERE transaction_id IS NOT NULL;
+CREATE INDEX idx_fees_billing_date ON fees(charged_at);
 
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS fees;
-
 DROP TABLE IF EXISTS plugin_policy_billing;
-
+DROP VIEW IF EXISTS billing_periods;
+DROP VIEW IF EXISTS fees_view;
 DROP TYPE IF EXISTS billing_frequency;
-
 DROP TYPE IF EXISTS fee_type;
+DROP INDEX IF EXISTS idx_fees_plugin_policy_billing_id;
+DROP INDEX IF EXISTS idx_fees_transaction_id;
+DROP INDEX IF EXISTS idx_fees_billing_date;
 
 -- +goose StatementEnd
+
+
--- types/policy.go
+++ types/policy.go
@@ -1,44 +1,35 @@
 package types
 
 import (
+	"encoding/base64"
+	"fmt"
+	"time"
+
 	"github.com/google/uuid"
+	rtypes "github.com/vultisig/recipes/types"
+	"google.golang.org/protobuf/encoding/protojson"
 )
+
+type Fee struct {
+	ID                    uuid.UUID  `json:"id"`
+	PluginPolicyBillingID uuid.UUID  `json:"plugin_policy_billing_id"`
+	TransactionID         uuid.UUID  `json:"transaction_id"`
+	Amount                int        `json:"amount"`
+	Type                  string     `json:"type"` // "tx", "recurring" or "once". Only availble on the fees_view table
+	CreatedAt             time.Time  `json:"created_at"`
+	ChargedAt             time.Time  `json:"charged_at"`
+	CollectedAt           *time.Time `json:"collected_at"`
+}
 
 type BillingPolicy struct {
 	ID        uuid.UUID `json:"id" validate:"required"`
-	Type      string    `json:"type" validate:"required"` // "tx", "recurring" or "once"
-	Frequency string    `json:"frequency"`                // only "monthly" for now
-	StartDate string    `json:"start_date"`               // Number of a month, e.g., "1" for the first month. Only allow 1 for now
+	Type      string    `json:"type" validate:"required"`   // "tx", "recurring" or "once"
+	Frequency string    `json:"frequency"`                  // only "monthly" for now
+	StartDate time.Time `json:"start_date"`                 // Number of a month, e.g., "1" for the first month. Only allow 1 for now
+	Amount    int       `json:"amount" validate:"required"` // Amount in the smallest unit, e.g., "1000000" for 0.01 VULTI
 }
 
 // This type should be used externally when creating or updating a plugin policy. It keeps the protobuf encoded billing recipe as a string which is used to verify a signature.
-type PluginPolicyCreateUpdate struct {
-	ID            uuid.UUID `json:"id" validate:"required"`
-	PublicKey     string    `json:"public_key" validate:"required"`
-	PluginID      PluginID  `json:"plugin_id" validate:"required"`
-	PluginVersion string    `json:"plugin_version" validate:"required"`
-	PolicyVersion string    `json:"policy_version" validate:"required"`
-	Signature     string    `json:"signature" validate:"required"`
-	Recipe        string    `json:"recipe" validate:"required"`         // base64 encoded recipe protobuf bytes
-	BillingRecipe string    `json:"billing_recipe" validate:"required"` // base64 encoded billing recipe protobuf bytes
-	Active        bool      `json:"active" validate:"required"`
-}
-
-func (p *PluginPolicyCreateUpdate) ToPluginPolicy() PluginPolicy {
-	return PluginPolicy{
-		ID:            p.ID,
-		PublicKey:     p.PublicKey,
-		PluginID:      p.PluginID,
-		PluginVersion: p.PluginVersion,
-		PolicyVersion: p.PolicyVersion,
-		Signature:     p.Signature,
-		Recipe:        p.Recipe,
-		Billing:       []BillingPolicy{}, // TODO garry This will be populated later
-		Active:        p.Active,
-	}
-}
-
-// PluginPolicy is our internal typing and return DTO for plugin policies. It shows the previously protobuf encoded billing recipe as a slice of BillingPolicy objects which closer aligns with the DB.
 type PluginPolicy struct {
 	ID            uuid.UUID       `json:"id" validate:"required"`
 	PublicKey     string          `json:"public_key" validate:"required"`
@@ -51,16 +42,35 @@ type PluginPolicy struct {
 	Active        bool            `json:"active" validate:"required"`
 }
 
-func (p *PluginPolicy) ToPluginPolicyCreateUpdate() PluginPolicyCreateUpdate {
-	return PluginPolicyCreateUpdate{
-		ID:            p.ID,
-		PublicKey:     p.PublicKey,
-		PluginID:      p.PluginID,
-		PluginVersion: p.PluginVersion,
-		PolicyVersion: p.PolicyVersion,
-		Signature:     p.Signature,
-		Recipe:        p.Recipe,
-		BillingRecipe: "", // TODO garry This will be populated later
-		Active:        p.Active,
+func (p *PluginPolicy) PopulateBilling() error {
+
+	p.Billing = []BillingPolicy{}
+
+	var recipe rtypes.Policy
+	policyBytes, err := base64.StdEncoding.DecodeString(p.Recipe)
+	if err != nil {
+		return fmt.Errorf("failed to decode policy recipe: %w", err)
 	}
+
+	if err := protojson.Unmarshal(policyBytes, &recipe); err != nil {
+		return fmt.Errorf("failed to unmarshal recipe: %w", err)
+	}
+
+	for _, feePolicy := range recipe.FeePolicies {
+		if feePolicy.Id == "" {
+			feePolicy.Id = uuid.New().String()
+		}
+		id, err := uuid.Parse(feePolicy.Id)
+		if err != nil {
+			return fmt.Errorf("failed to parse fee policy ID: %w", err)
+		}
+		p.Billing = append(p.Billing, BillingPolicy{
+			ID:        id,
+			Type:      string(feePolicy.Type),
+			Frequency: string(feePolicy.Frequency),
+			StartDate: feePolicy.StartDate.AsTime(),
+			Amount:    int(feePolicy.Amount),
+		})
+	}
+	return nil
 }

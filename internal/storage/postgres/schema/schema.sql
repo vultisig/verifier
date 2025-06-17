aa1e213fefc323f60a91076c8fc374b0ea4f2735
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-06-18 00:46:40 +0300

    GetTxsInTimeRange

--- internal/storage/postgres/schema/schema.sql
+++ internal/storage/postgres/schema/schema.sql
@@ -291,7 +291,7 @@ CREATE INDEX "idx_reviews_plugin_id" ON "reviews" USING "btree" ("plugin_id");
 
 CREATE INDEX "idx_reviews_public_key" ON "reviews" USING "btree" ("public_key");
 
-CREATE INDEX "idx_tx_indexer_key" ON "tx_indexer" USING "btree" ("chain_id", "plugin_id", "policy_id", "token_id", "to_public_key");
+CREATE INDEX "idx_tx_indexer_key" ON "tx_indexer" USING "btree" ("chain_id", "plugin_id", "policy_id", "token_id", "to_public_key", "created_at");
 
 CREATE INDEX "idx_tx_indexer_status_onchain_lost" ON "tx_indexer" USING "btree" ("status_onchain", "lost");
 

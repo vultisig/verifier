60a6a4fdb0b6ab81a057ba7dbfe2cde12d43bf2a
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-06-29 17:02:28 +0330

    Add migration for transaction_history

---
+++ internal/storage/postgres/migrations/verifier/20250629131507_transaction_history.sql
@@ -0,0 +1,31 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TYPE transaction_status AS ENUM (
+  'SIGNING_IN_PROGRESS',
+  'SIGNING_FAILED',
+  'SIGNED',
+  'BROADCAST',
+  'PENDING',
+  'MINED',
+  'REJECTED'
+);
+CREATE TABLE IF NOT EXISTS transaction_history (
+    id UUID PRIMARY KEY,
+    policy_id UUID NOT NULL,
+    tx_body TEXT NOT NULL,
+    tx_hash TEXT NOT NULL,
+    status TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    metadata JSONB NOT NULL,
+    error_message TEXT,
+    CONSTRAINT fk_plugin_policy FOREIGN KEY (policy_id)
+        REFERENCES plugin_policies(id) ON DELETE CASCADE
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS transaction_history;
+DROP TYPE IF EXISTS transaction_status;
+-- +goose StatementEnd

50c2e073c6238afcda3bdeafd0352e042d78fb5e
Raghav Sood <r@raghavsood.com>
2025-07-02 03:50:12 +0800

    smoke: hurl remaining public APIs w/o reviews

--- internal/types/tag.go
+++ internal/types/tag.go
@@ -2,7 +2,7 @@ package types
 
 import "time"
 
-// Tag represents a plugin tag with unique identifier, name and color
+// Tag represents a plugin tag with unique identifier, name
 type Tag struct {
 	ID        string    `json:"id" validate:"required"`
 	Name      string    `json:"name" validate:"required,min=2,max=50"`

e796168b1c77b9b21214238cd2bd5f021f4d33b9
Garry Sharp <>
2025-07-10 13:54:13 +0400

    ✅ make pricing data for fees much smaller for testing

--- testdata/seeds/01_pricings.sql
+++ testdata/seeds/01_pricings.sql
@@ -3,11 +3,14 @@ INSERT INTO pricings (id, type, frequency, amount, metric) VALUES
 ('00000000-0000-0000-0000-000000000001', 'free', NULL, 0.00, 'fixed'),
 
 -- Per-transaction pricing
-('00000000-0000-0000-0000-000000000002', 'per-tx', NULL, 100.00, 'fixed'),
+('00000000-0000-0000-0000-000000000002', 'per-tx', NULL, 0.01, 'fixed'),
+
+-- Single pricing for payroll plugin
+('20000000-0000-0000-0000-000000000003', 'single', NULL, 0.05, 'fixed'),
 
 -- Monthly recurring pricing
-('00000000-0000-0000-0000-000000000003', 'recurring', 'monthly', 1000.00, 'fixed'),
+('00000000-0000-0000-0000-000000000003', 'recurring', 'monthly', 0.03, 'fixed'),
 
 -- Weekly recurring pricing  
-('00000000-0000-0000-0000-000000000004', 'recurring', 'weekly', 250.00, 'fixed')
+('00000000-0000-0000-0000-000000000004', 'recurring', 'weekly', 0.02, 'fixed')
 ON CONFLICT (id) DO NOTHING;
---
+++ testdata/seeds/06_fees.sql
@@ -0,0 +1,7 @@
+INSERT INTO plugin_policy_billing (id, "type", frequency, start_date, amount, plugin_policy_id)
+VALUES ('20000000-2000-2000-0000-000000000222', 'once', NULL, CURRENT_DATE, 0.05, '20000000-0000-0000-0000-000000000002') 
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO fees (id, plugin_policy_billing_id, amount)
+VALUES ('20000000-2000-2000-2222-000000000000', '20000000-2000-2000-0000-000000000222', 0.05)
+ON CONFLICT (id) DO NOTHING;

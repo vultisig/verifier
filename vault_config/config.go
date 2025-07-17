caf1cd2b14508cd5fcf653f23f5a8fb4fd3d8d43
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-07-17 21:32:29 +0300

    fix keysign

--- vault_config/config.go
+++ vault_config/config.go
@@ -3,10 +3,6 @@ package vault_config
 type Config struct {
 	Relay struct {
 		Server string `mapstructure:"server" json:"server"`
-		// Encrypt MPC plugin.worker<>relay<>verifier.worker
-		// Doesn't need to set secret on verifier.worker side, because it gets it alongside with KeysignRequest
-		// by private plugin->verifier API call (put to queue)
-		EncryptionSecret string `mapstructure:"encryption_secret" json:"encryption_secret,omitempty"`
 	} `mapstructure:"relay" json:"relay,omitempty"`
 	LocalPartyPrefix string `mapstructure:"local_party_prefix" json:"local_party_prefix,omitempty"`
 	QueueEmailTask   bool   `mapstructure:"queue_email_task" json:"queue_email_task,omitempty"`

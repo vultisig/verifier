e3be4ef351226ee44bc24a5079ead968235273ef
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-07-19 14:34:52 +0300

    fix keysign

--- vault/keysign.go
+++ vault/keysign.go
@@ -133,41 +133,6 @@ func (t *DKLSTssService) ProcessDKLSKeysign(req types.KeysignRequest) (map[strin
 
 	return result, nil
 }
-func (t *DKLSTssService) keysignWithRetry(sessionID string,
-	hexEncryptionKey string,
-	publicKey string,
-	isEdDSA bool,
-	message string,
-	derivePath string,
-	localPartyID string,
-	keysignCommittee []string) (*tss.KeysignResponse, error) {
-	for i := 0; i < 3; i++ {
-		keysignResult, err := t.keysign(sessionID,
-			hexEncryptionKey,
-			publicKey,
-			isEdDSA,
-			message,
-			derivePath,
-			localPartyID,
-			keysignCommittee, i)
-		if err != nil {
-			t.logger.WithFields(logrus.Fields{
-				"session_id":        sessionID,
-				"public_key_ecdsa":  publicKey,
-				"message":           message,
-				"derive_path":       derivePath,
-				"local_party_id":    localPartyID,
-				"keysign_committee": keysignCommittee,
-				"attempt":           i,
-			}).Error(err)
-			time.Sleep(50 * time.Millisecond)
-			continue
-		} else {
-			return keysignResult, nil
-		}
-	}
-	return nil, fmt.Errorf("fail to keysign after max retry")
-}
 
 func (t *DKLSTssService) keysign(sessionID string,
 	hexEncryptionKey string,

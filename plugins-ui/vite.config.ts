a8c0782414aadf9525645f32d955b7e680638fd6
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-07-22 10:54:33 +0330

    Update vite.config.ts

--- plugins-ui/vite.config.ts
+++ plugins-ui/vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [nodePolyfills(), react(), tsconfigPaths()],
+  server: {
+    allowedHosts: ['store.vultisigplugin.app'], // 👈 Add this line
+  },
 });

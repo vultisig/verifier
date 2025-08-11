e6cef9d1b324e80ff8b18497662c4de5d68fe350
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-08-11 21:33:19 +0300

    /recipe-specification/suggest

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -8,4 +8,21 @@ import (
 type Spec interface {
 	GetRecipeSpecification() (*rtypes.RecipeSchema, error)
 	ValidatePluginPolicy(policyDoc types.PluginPolicy) error
+	Suggest(configuration map[string]any) (*rtypes.PolicySuggest, error)
+}
+
+// Unimplemented for backward compatibility in the case of new interface methods
+type Unimplemented struct {
+}
+
+func (*Unimplemented) GetRecipeSpecification() (*rtypes.RecipeSchema, error) {
+	return nil, nil
+}
+
+func (*Unimplemented) ValidatePluginPolicy(_ types.PluginPolicy) error {
+	return nil
+}
+
+func (*Unimplemented) Suggest(map[string]any) (*rtypes.PolicySuggest, error) {
+	return nil, nil
 }

5a0509fc7e87d13e23250d6d093c16ae015bb454
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-08-19 12:33:18 +0330

    Remove plugins ui

--- plugins-ui/src/utils/constants/theme.ts
+++
@@ -1,5 +0,0 @@
-export const themes = ["dark", "light"] as const;
-
-export type Theme = (typeof themes)[number];
-
-export const defaultTheme: Theme = "light";
--- plugins-ui/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-  plugins: [nodePolyfills(), react(), tsconfigPaths()],
-  server: { allowedHosts: true },
-});

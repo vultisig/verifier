91605fed78a858d1363f09f96264e05c1f02b5ff
webpiratt <213060745+webpiratt@users.noreply.github.com>
2025-10-09 21:50:38 +0300

    plugins yaml

--- testdata/seeds/04_tags.sql
+++ testdata/seeds/04_tags.sql
@@ -1,8 +1,8 @@
-INSERT INTO tags (id, name, created_at) VALUES 
+INSERT INTO tags (id, name, created_at) VALUES
 ('00000000-0000-0000-0000-000000000001', 'Trading', NOW()),
 ('00000000-0000-0000-0000-000000000002', 'Operations', NOW()) ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO plugin_tags (plugin_id, tag_id) VALUES 
+INSERT INTO plugin_tags (plugin_id, tag_id) VALUES
 ('vultisig-dca-0000', '00000000-0000-0000-0000-000000000001'),
 ('vultisig-copytrader-0000', '00000000-0000-0000-0000-000000000001'),
-('vultisig-payroll-0000', '00000000-0000-0000-0000-000000000002') ON CONFLICT (plugin_id, tag_id) DO NOTHING;
+('vultisig-payroll-0000', '00000000-0000-0000-0000-000000000002') ON CONFLICT (tag_id) DO NOTHING;

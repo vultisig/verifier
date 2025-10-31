e126d89ea9c0c6cb4d847827a8533158d5e6383f
Shevchuk <evgen.sheff@gmail.com>
2025-10-31 12:22:09 +0700

    plugin target fix

--- internal/tx_indexer/indexer.go
+++ internal/tx_indexer/indexer.go
@@ -77,7 +77,7 @@ func (fi *FeeIndexer) updateTxStatus(ctx context.Context, tx storage.Tx) error {
 		return fmt.Errorf("new status is nil")
 	}
 
-	if tx.PluginID == types.PluginVultisigFees_feee && *newStatus == rpc.TxOnChainSuccess {
+	if tx.PluginID != types.PluginVultisigFees_feee && *newStatus == rpc.TxOnChainSuccess {
 		err = fi.db.WithTransaction(ctx, func(ctx context.Context, dbTx pgx.Tx) error {
 			var err error
 

6390481d661e5fd9c898f41ae81e0745ec644226
Shevchuk <evgen.sheff@gmail.com>
2025-11-24 16:24:52 +0700

    removed down

--- internal/storage/postgres/migrations/verifier/20251121000001_new_plugin.sql
+++ internal/storage/postgres/migrations/verifier/20251121000001_new_plugin.sql
@@ -15,19 +15,3 @@ VALUES
 -- Per-transaction pricing 1c
 ('00000000-0000-0000-0000-000000000006', 'vultisig-recurring-sends-0000', 'usdc', 'per-tx', NULL, 1e4,
  'fixed') ON CONFLICT (id) DO NOTHING;
-
-INSERT INTO plugin_tags (plugin_id, tag_id)
-VALUES ('vultisig-recurring-sends-0000', '00000000-0000-0000-0000-000000000001') ON CONFLICT (plugin_id, tag_id) DO NOTHING;
-
--- +goose Down
-DELETE
-FROM pricings
-WHERE id = '00000000-0000-0000-0000-000000000006';
-
-DELETE
-FROM plugin_tags
-WHERE plugin_id = 'vultisig-recurring-sends-0000';
-
-DELETE
-FROM plugins
-WHERE id = 'vultisig-recurring-sends-0000';

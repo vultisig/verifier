ec15afbb04616888e5377e740eee46e142f481e7
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-12-01 11:50:48 +0330

    Update banner and icon for plugins (#413)
    * Update banner and icon for plugins
    
    * Update 20251201102000_update_icons_banners_plugins.sql
    
    * Consolidate plugin logo URL updates in migration

---
+++ internal/storage/postgres/migrations/verifier/20251201102000_update_icons_banners_plugins.sql
@@ -0,0 +1,36 @@
+-- +goose Up
+-- +goose StatementBegin
+BEGIN;
+
+UPDATE plugins
+SET
+    logo_url = 'https://raw.githubusercontent.com/vultisig/verifier/main/assets/plugins/recurring_send/icon.jpg',
+    thumbnail_url = 'https://raw.githubusercontent.com/vultisig/verifier/main/assets/plugins/recurring_send/banner.jpg'
+WHERE id = 'vultisig-recurring-sends-0000';
+
+UPDATE plugins
+SET
+    logo_url = 'https://raw.githubusercontent.com/vultisig/verifier/main/assets/plugins/recurring_swap/icon.jpg',
+    thumbnail_url = 'https://raw.githubusercontent.com/vultisig/verifier/main/assets/plugins/recurring_swap/banner.jpg'
+WHERE id = 'vultisig-dca-0000';
+
+UPDATE plugins
+SET
+    logo_url = 'https://raw.githubusercontent.com/vultisig/verifier/main/assets/plugins/payment/icon.jpg',
+    thumbnail_url = 'https://raw.githubusercontent.com/vultisig/verifier/main/assets/plugins/payment/banner.jpg'
+WHERE id = 'vultisig-fees-feee';
+
+COMMIT;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+BEGIN;
+
+UPDATE plugins
+SET
+    logo_url = '',
+    thumbnail_url = ''
+WHERE id IN ('vultisig-recurring-sends-0000', 'vultisig-dca-0000', 'vultisig-fees-feee');
+COMMIT;
+-- +goose StatementEnd

c0dd209b0e2008accecf0aee9805b33c6832c684
JP <jpthor@protonmail.com>
2026-01-16 12:11:30 +1100

    fix: remove down migrations to avoid data loss

--- plugin/policy/policy_pg/migrations/20260114120000_add_deactivation_reason.sql
+++ plugin/policy/policy_pg/migrations/20260114120000_add_deactivation_reason.sql
@@ -4,6 +4,3 @@ ALTER TABLE plugin_policies ADD COLUMN deactivation_reason TEXT DEFAULT NULL;
 -- +goose StatementEnd
 
 -- +goose Down
--- +goose StatementBegin
-ALTER TABLE plugin_policies DROP COLUMN IF EXISTS deactivation_reason;
--- +goose StatementEnd

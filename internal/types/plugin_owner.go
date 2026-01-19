235c07e143915cca50731f9e2cc41e4c52dc4e3c
4others <44651681+4others@users.noreply.github.com>
2026-01-19 12:47:37 +0400

    apply go fmt

--- internal/types/plugin_owner.go
+++ internal/types/plugin_owner.go
@@ -8,10 +8,10 @@ import (
 
 type PluginOwner struct {
 	PluginID         vtypes.PluginID `json:"plugin_id"`
-	PublicKey        string         `json:"public_key"`
-	Active           bool           `json:"active"`
-	AddedVia         string         `json:"added_via"`
-	AddedByPublicKey string         `json:"added_by_public_key,omitempty"`
-	CreatedAt        time.Time      `json:"created_at"`
-	UpdatedAt        time.Time      `json:"updated_at"`
+	PublicKey        string          `json:"public_key"`
+	Active           bool            `json:"active"`
+	AddedVia         string          `json:"added_via"`
+	AddedByPublicKey string          `json:"added_by_public_key,omitempty"`
+	CreatedAt        time.Time       `json:"created_at"`
+	UpdatedAt        time.Time       `json:"updated_at"`
 }

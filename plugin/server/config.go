207a0a0296de6ae8b64b485ed5b65bbf3524a3b3
JP <jpthor@protonmail.com>
2026-01-26 12:00:11 +1100

    fix: remove envconfig tags from server config

--- plugin/server/config.go
+++ plugin/server/config.go
@@ -1,8 +1,8 @@
 package server
 
 type Config struct {
-	Host             string `mapstructure:"host" json:"host,omitempty" envconfig:"SERVER_HOST"`
-	Port             int64  `mapstructure:"port" json:"port,omitempty" envconfig:"SERVER_PORT"`
-	EncryptionSecret string `mapstructure:"encryption_secret" json:"encryption_secret,omitempty" envconfig:"SERVER_ENCRYPTIONSECRET"`
-	TaskQueueName    string `mapstructure:"task_queue_name" json:"task_queue_name,omitempty" envconfig:"SERVER_TASK_QUEUE_NAME"`
+	Host             string `mapstructure:"host" json:"host,omitempty"`
+	Port             int64  `mapstructure:"port" json:"port,omitempty"`
+	EncryptionSecret string `mapstructure:"encryption_secret" json:"encryption_secret,omitempty"`
+	TaskQueueName    string `mapstructure:"task_queue_name" json:"task_queue_name,omitempty"`
 }
